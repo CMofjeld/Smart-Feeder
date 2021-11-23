1f427df4f96872f4c66a3da6f44bb44e56916ba1
CMofjeld <cmofjeld@uw.edu>
2021-11-22 16:45:19 -0800

    stream analytics to store visit data in database

---
+++ StoreBirdVisitsInDB/Inputs/Devices.snapshot.sql
@@ -0,0 +1,3 @@
+SELECT id, device_name
+From dbo.devices;
+--FOR SYSTEM_TIME AS OF @snapshotTime --Optional, available if Device is a temporal table
---
+++ StoreBirdVisitsInDB/Inputs/Species.snapshot.sql
@@ -0,0 +1,3 @@
+SELECT id, common_name
+From dbo.bird_species;
+--FOR SYSTEM_TIME AS OF @snapshotTime --Optional, available if Device is a temporal table
